800417cb9a95193ac1f3f26316bd097fa3432d27
Austin Meyer <frizzkitten@gmail.com>
2020-02-19 17:14:17 -0600

    getting ready to make actual calculations

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ let socket;
 const endpoint = "http://localhost:3001/"; // Update 3001 with port on which backend-my-app/server.js is running.
 socket = socketIOClient(endpoint);
 
+// send the calculation to the database
+const sendCalculation = calculation =>
+    socket.emit("send_calculation", calculation);
+
 class App extends Component {
     constructor(props) {
         super(props);
@@ -31,24 +35,15 @@ class App extends Component {
         socket.off("change_calc_data");
     }
 
-    // send the calculation to the database
-    sendCalculation = calculation => {
-        socket.emit("send_calculation", calculation);
-    };
+    // do the math and then send the resulting string to the server
+    calculate = () => {
+        // get the input string
+        const input = document.getElementById("calc-input").value;
 
-    calculationRows = () => {
-        return this.state.calculations.map(calculation => {
-            return (
-                <tr key={calculation._id}>
-                    <td>{calculation.equation}</td>
-                </tr>
-            );
-        });
-    };
+        let equation = input;
 
-    // update the input and state when the input box is changed
-    onTextChange = event => {
-        this.setState({ text: event.currentTarget.value });
+        // send the full equation string to the backend
+        sendCalculation(equation);
     };
 
     render() {
@@ -57,29 +52,32 @@ class App extends Component {
                 <Container>
                     <h2 className="h2Class">Big Brother Calculator</h2>
 
-                    <input
-                        type="text"
-                        value={this.state.text}
-                        onChange={this.onTextChange}
-                    />
-                    <Button
-                        onClick={() => this.sendCalculation(this.state.text)}
-                    >
-                        Send
-                    </Button>
+                    <input type="text" id="calc-input" />
+                    <Button onClick={this.calculate}>Send</Button>
 
-                    <Table striped id="table-to-xls">
-                        <thead>
-                            <tr>
-                                <th>Prior Calculations</th>
-                            </tr>
-                        </thead>
-                        <tbody>{this.calculationRows()}</tbody>
-                    </Table>
+                    <CalculationTable calculations={this.state.calculations} />
                 </Container>
             </div>
         );
     }
 }
+
+// shows the previous 10 calculations
+const CalculationTable = ({ calculations }) => (
+    <Table striped id="table-to-xls">
+        <thead>
+            <tr>
+                <th>Prior Calculations</th>
+            </tr>
+        </thead>
+        <tbody>
+            {calculations.map(calculation => (
+                <tr key={calculation._id}>
+                    <td>{calculation.equation}</td>
+                </tr>
+            ))}
+        </tbody>
+    </Table>
+);
 
 export default App;
