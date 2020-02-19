e12e5208b881eac6890ddfe73623ba55ef5a2db6
Austin Meyer <frizzkitten@gmail.com>
2020-02-19 11:15:05 -0600

    first commit

---
+++ backend-my-app/schemas/FoodItemSchema.js
@@ -0,0 +1,8 @@
+// var FoodItemSchema = new mongoose.Schema({
+//     name: String,
+//     predQty: Number,
+//     prodQty: Number,
+//     ordQty: Number
+// });
+//
+// export default mongoose.model("FoodItem", FoodItemSchema);
