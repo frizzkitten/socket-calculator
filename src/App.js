ae76d0ee0d1cb3935f53e545d61accbe73f0bb03
Austin Meyer <frizzkitten@gmail.com>
2020-02-19 21:07:07 -0600

    can calculate correctly based on input

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Button, Table, Container } from "reactstrap";
 
 import "./App.css";
 
+import { getCalculationParts, calculateFromParts } from "./util";
+
 import socketIOClient from "socket.io-client";
 let socket;
 const endpoint = "http://localhost:3001/"; // Update 3001 with port on which backend-my-app/server.js is running.
@@ -11,13 +13,6 @@ socket = socketIOClient(endpoint);
 // send the calculation to the database
 const sendCalculation = calculation =>
     socket.emit("send_calculation", calculation);
-
-const validNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-const validOperators = ["x", "-", "/", "+"];
-const validSymbols = validNumbers.concat(validOperators);
-// returns "operator" if it's a symbol, otherwise returns "number"
-// const getType = part => (validOperators.includes(part) ? "operator" : "number");
-const getIsOperator = part => validOperators.includes(part);
 
 class App extends Component {
     constructor(props) {
@@ -48,103 +43,19 @@ class App extends Component {
         let input = document.getElementById("calc-input").value;
         if (input.length === 0) return;
 
-        // remove all the spaces
-        input = input.replace(/\s/g, "");
-
-        // check if there are invalid characters
-        if (input.split("").some(c => !validSymbols.includes(c))) {
-            return this.setState({
-                error: "Only numbers, spaces, and x+-/ are allowed."
-            });
-        }
-
-        let partIsNumber = true;
-        let parts = [];
-        while (input.length > 0) {
-            // part should be a number
-            if (partIsNumber) {
-                const number = parseInt(input, 10);
-
-                if (isNaN(number)) {
-                    return this.setState({
-                        error: "Can't start with an operator."
-                    });
-                }
-
-                const numberString = `${number}`;
-                // if the input doesn't start with this number,
-                // there must be some operator before it,
-                // which means there were two operators in a row
-                if (!input.startsWith(numberString)) {
-                    return this.setState({
-                        error: "Can't have two operators in a row."
-                    });
-                }
-                // add the number and remove it from the input string
-                parts.push(number);
-                input = input.substring(numberString.length);
-                // set the next part to be an operator
-                partIsNumber = false;
-            }
-            // add the operator, then say that the next thing should be a number
-            else {
-                if (input.length === 1)
-                    return this.setState({
-                        error: "Can't end with an operator."
-                    });
-
-                parts.push(input.charAt(0));
-                input = input.substring(1);
-                partIsNumber = true;
-            }
+        // get an array of numbers and operators
+        try {
+            var parts = getCalculationParts(input);
+        } catch (error) {
+            if (typeof error === "string") return this.setState({ error });
+            else return this.setState({ error: "Invalid calculation." });
         }
 
         console.log("parts: ", parts);
+        // calculate the result from those parts
+        const result = calculateFromParts(parts);
 
-        // let parts = [];
-        // const numChars = input.length;
-        // let charIndex = 0;
-        // while (charIndex < numChars) {
-        //     // get the current character
-        //     const char = input.charAt(charIndex);
-        //     // find out if the character is an operator or number
-        //     const isOperator = getIsOperator(char);
-        //
-        //     // deal with an operator
-        //     if (isOperator) {
-        //         // if the last character is an operator, this is invalid
-        //         if (charIndex === numChars - 1)
-        //             return this.setState({ error: "Can't end in an operator" });
-        //         // if the first character is an operator ...
-        //         else if (charIndex === 0) {
-        //             // ... if it's not a -, then it's invalid
-        //             if (char !== "-")
-        //                 return this.setState({
-        //                     error: "Can't start with an operator"
-        //                 });
-        //         }
-        //
-        //
-        //         // if there are two operators in a row ...
-        //         if ()
-        //     }
-        //
-        //     charIndex++
-        // }
-
-        // check if the first or last characters are operators
-        // let parts = input.split(" ");
-        // parts = parts.filter(part => part.length > 0);
-        // if (
-        //     getType(parts[0]) === "operator" ||
-        //     getType(parts[parts.length - 1]) === "operator"
-        // ) {
-        //     return this.setState({
-        //         error: "Cannot start or end with an operator."
-        //     });
-        // }
-        // check if there are any numbers next to each other or symbols
-        // next to each other
+        console.log("result: ", result);
 
         let equation = input;
 
---
+++ src/util.js
@@ -0,0 +1,119 @@
+const validNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const validOperators = ["x", "-", "/", "+"];
+const validSymbols = validNumbers.concat(validOperators);
+
+// takes an input string and returns an array of numbers and operators
+function getCalculationParts(input) {
+    // remove all the spaces
+    input = input.replace(/\s/g, "");
+
+    // check if there are invalid characters
+    if (input.split("").some(c => !validSymbols.includes(c)))
+        throw "Only numbers, spaces, and x+-/ are allowed.";
+
+    let partIsNumber = true;
+    let parts = [];
+    while (input.length > 0) {
+        // part should be a number
+        if (partIsNumber) {
+            const number = parseInt(input, 10);
+            const numberString = `${number}`;
+
+            // if it's NaN that means the first number couldn't be read,
+            // so it must be an operator
+            if (isNaN(number)) throw "Can't start with an operator.";
+
+            // if the input doesn't start with this number,
+            // there must be some operator before it,
+            // which means there were two operators in a row
+            if (!input.startsWith(numberString))
+                throw "Can't have two operators in a row.";
+
+            // add the number and remove it from the input string
+            parts.push(number);
+            input = input.substring(numberString.length);
+            // set the next part to be an operator
+            partIsNumber = false;
+        }
+        // add the operator, then say that the next thing should be a number
+        else {
+            if (input.length === 1) throw "Can't end with an operator.";
+
+            parts.push(input.charAt(0));
+            input = input.substring(1);
+            partIsNumber = true;
+        }
+    }
+
+    return parts;
+}
+
+// operation functions
+const subtract = (a, b) => a - b;
+const add = (a, b) => a + b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+// get a result from a list of numbers and operators;
+// it's known that every even index will be a number
+// and every odd index will be an operator
+function calculateFromParts(p) {
+    // make a new copy of the parts array
+    let parts = p.slice(0);
+
+    // go through each operator
+    while (parts.length > 1) {
+        // multiplication and division
+        parts = doOperations(parts, [
+            { symbol: "x", operation: multiply },
+            { symbol: "/", operation: divide }
+        ]);
+        // addition and subtraction
+        parts = doOperations(parts, [
+            { symbol: "+", operation: add },
+            { symbol: "-", operation: subtract }
+        ]);
+    }
+
+    return parts[0];
+}
+
+// takes in an array of numbers and symbols and a list
+// of wanted operations to do in the form of
+// [{ symbol: "x", operations: multiply }, ...]
+function doOperations(p, operations) {
+    let parts = p.slice(0);
+
+    let partIndex = 1;
+    while (partIndex < parts.length) {
+        const symbol = parts[partIndex];
+
+        // see if the symbol is one of the wanted ones
+        let o;
+        operations.forEach(op => {
+            if (op.symbol === symbol) o = op;
+        });
+
+        // do the operation if the symbol is wanted
+        if (o) {
+            // calculate the result of multiplying or dividing the
+            // numbers before and after the operator
+            const num1 = parts[partIndex - 1];
+            const num2 = parts[partIndex + 1];
+            const result = o.operation(num1, num2);
+
+            parts = parts
+                .slice(0, partIndex - 1)
+                .concat(result)
+                .concat(parts.slice(partIndex + 2));
+
+            console.log("updated parts: ", parts);
+        }
+        // otherwise move on to the next operator
+        else partIndex += 2;
+    }
+
+    return parts;
+}
+
+export { getCalculationParts, calculateFromParts };
