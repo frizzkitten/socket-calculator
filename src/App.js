a0e2120c5f2d25e54655fa35ad630cea7509add8
Austin Meyer <frizzkitten@gmail.com>
2020-02-26 15:14:12 -0600

    addNumber

--- src/App.js
+++ src/App.js
@@ -14,6 +14,30 @@ socket = socketIOClient(endpoint);
 const sendCalculation = calculation =>
     socket.emit("send_calculation", calculation);
 
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const buttonTexts = [
+    "(",
+    ")",
+    "%",
+    "C",
+    "7",
+    "8",
+    "9",
+    "/",
+    "4",
+    "5",
+    "6",
+    "x",
+    "1",
+    "2",
+    "3",
+    "-",
+    "0",
+    ".",
+    "=",
+    "+"
+];
+
 class App extends Component {
     constructor(props) {
         super(props);
@@ -80,7 +104,107 @@ class App extends Component {
     onButtonClick = event => {
         const text = event.currentTarget.value;
         console.log("Button clicked: ", text);
+        let { calcParts } = this.state;
+
+        switch (text) {
+            case "0":
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+                this.addNumber(calcParts, text);
+                break;
+            case ".":
+                this.addDecimal(calcParts);
+                break;
+            case "C":
+                this.backspace(calcParts);
+                break;
+            case "(":
+            case ")":
+                this.addParenthesis(calcParts, text);
+                break;
+            case "%":
+            case "+":
+            case "x":
+            case "/":
+                this.addSymbol(calcParts, text);
+                break;
+            case "-":
+                this.addSubtraction(calcParts);
+                break;
+            case "=":
+                this.calculate(calcParts);
+                break;
+            default:
+                return this.setState({
+                    error: "Something has gone horribly wrong."
+                });
+        }
     };
+
+    // adds a number to the calculation
+    addNumber = (calcParts, text) => {
+        // if there is nothing yet, just add the number
+        if (calcParts.length == 0) calcParts = [text];
+        else {
+            const lastIndex = calcParts.length - 1;
+            const lastPart = calcParts[lastIndex];
+            const lastChar = lastPart.charAt(lastPart.length - 1);
+
+            // if last was and end parenthesis, include a multiply sign
+            if (lastPart === ")") calcParts = calcParts.concat(["x", text]);
+            // if last was a number or dot or negative symbol, add this to that
+            else if (
+                numbers.includes(lastChar) ||
+                lastChar === "." ||
+                lastChar === "_"
+            )
+                calcParts[lastIndex] = lastPart + text;
+            // otherwise just add the number
+            else calcParts.push(text);
+        }
+
+        this.setState({ calcParts });
+    };
+
+    addDecimal = calcParts => {
+        // TODO
+    };
+
+    // removes the last char from the calculation
+    backspace = calcParts => {
+        // TODO
+    };
+
+    // adds a forward or back parenthesis to the calculation
+    addParenthesis = (calcParts, paren) => {
+        // TODO
+    };
+
+    // adds an x, +, %, or / to the calculation
+    addSymbol = (calcParts, text) => {
+        // TODO
+    };
+
+    // add a - to the calculation
+    addSubtraction = calcParts => {
+        // TODO
+    };
+
+    // calculate the end result and send it to the backend
+    calculate = calcParts => {
+        // TODO
+    };
+
+    showState = () => console.log(this.state.calcParts);
+
+    // TODO CHECK FOR 0 / 0, ANYTHING / 0, DECIMALS
 
     render() {
         const { error, calculations } = this.state;
@@ -88,6 +212,8 @@ class App extends Component {
             <div className="App">
                 <Container>
                     <h2 className="header">Big Brother Calculator</h2>
+
+                    <button onClick={this.showState}>state</button>
 
                     {!!error && <div className="error">{error}</div>}
                     <input type="text" id="calc-input" className="calc-input" />
@@ -101,29 +227,6 @@ class App extends Component {
         );
     }
 }
-
-const buttonTexts = [
-    "(",
-    ")",
-    "%",
-    "C",
-    "7",
-    "8",
-    "9",
-    "/",
-    "4",
-    "5",
-    "6",
-    "x",
-    "1",
-    "2",
-    "3",
-    "-",
-    "0",
-    ".",
-    "=",
-    "+"
-];
 
 const CalcButtons = ({ onButtonClick }) => (
     <div className="calc-buttons">
